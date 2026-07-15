aad85a1d075b302e10787be875c216dc3c604f2b
So1968 <sofia.rios@gmx.fr>
2026-07-15 13:43:18 +0200

    Applique le correctif ergonomique accompagnement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
 import "./styles.css";
 import "./continuiteErgo.css";
 import "./parcoursReferentiel.css";
 import "./diagnosticReferentiel.css";
 import "./tnsErgo.css";
 import "./echeancesVigilances.css";
+import "./accompagnementClean.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
