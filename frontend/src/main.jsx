72caccad8fedcc26d1e1bc703b2b429139d48bff
So1968 <sofia.rios@gmx.fr>
2026-07-15 11:03:07 +0200

    Retire les surcouches de lisibilité trop grandes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-﻿import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
 import "./styles.css";
 import "./continuiteErgo.css";
 import "./parcoursReferentiel.css";
 import "./diagnosticReferentiel.css";
 import "./tnsErgo.css";
 import "./echeancesVigilances.css";
-import "./darkMode.css";
-import "./readableLargeText.css";
-import "./caravaneLisibilite.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
